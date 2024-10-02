d74c9e642404c956108631ae5e99b78e76dd22da
jaelps <jaelsonpsilva18@gmail.com>
2024-10-02 09:35:32 -0300

    iniciando debug e tratando dados de entrada

--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ Pessoa p1 = new Pessoa();
 
 p1.Nome = "Jaelson";
 p1.Idade = 30;
+p1.Apresentar();
--- obj/Debug/net8.0/exemploEsplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/exemploEsplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploEsplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c619ec4fe1093c4574e56f482ea2a55700ecbed")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploEsplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploEsplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
