70a5af5b3dc9f4a59d9869b695b2c65a77e32980
jaelps <jaelsonpsilva18@gmail.com>
2024-10-03 20:41:04 -0300

    teste de funcionamento

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿using exemploEsplorando.Models;
 
-Pessoa p1 = new Pessoa();
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Jaelson";
+//p1.Idade = 20;
+//p1.Apresentar();
 
+Pessoa p1 = new Pessoa();
 p1.Nome = "Jaelson";
-p1.Idade = 20;
-p1.Apresentar();
+
+
+Pessoa p2 = new Pessoa();
+p1.Nome = "Pereira";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
