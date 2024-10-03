02be55a3d82c60da5fccbb50452da895ece2f49f
jaelps <jaelsonpsilva18@gmail.com>
2024-10-03 07:50:27 -0300

    criando nova classe "Curso" o projeto

--- obj/Debug/net8.0/exemploEsplorando.AssemblyInfo.cs
+++ obj/Debug/net8.0/exemploEsplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemploEsplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74c9e642404c956108631ae5e99b78e76dd22da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c712a47444a59f59cefbc5e27293b9c3712bd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemploEsplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemploEsplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
